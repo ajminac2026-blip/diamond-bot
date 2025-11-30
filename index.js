dc8b78b3e5b1a71731b380f9689cd059461e9383
ajminac2026-blip <ajminac2026@gmail.com>
2025-11-30 14:40:55 +0600

    Fix Chromium sandbox issue for Replit deployment

--- index.js
+++ index.js
@@ -31,7 +31,18 @@ db.initializeUsers();
 db.initializeCleanup();
 
 const client = new Client({
-    authStrategy: new LocalAuth()
+    authStrategy: new LocalAuth(),
+    puppeteer: {
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--no-first-run',
+            '--no-zygote',
+            '--disable-gpu'
+        ]
+    }
 });
 
 let botIsReady = false; // Flag to track bot ready state
